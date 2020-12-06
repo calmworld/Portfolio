970483e8c25fc9bf94a350308c8cfb1cb718dfcb
Sarah <sarah.alhuriz@hotmail.com>
2020-12-06 13:02:03 -0700

    changed style

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Home() {
   return (
     <div>
-        <h1>Hello</h1>
+        <h1 className="hello">Hello World</h1>
     </div>
   )
 }
--- src/components/ParticleComp.jsx
+++ src/components/ParticleComp.jsx
@@ -9,92 +9,135 @@ function ParticleComp() {
             left: 0,
             width: "100%",
             height: "100%"
-          }}>
+        }}>
             <Particles
                 id="tsparticles"
                 options={{
-                    background: {
-                        color: {
-                            value: "#000000",
-                        },
-                    },
                     fpsLimit: 60,
-                    interactivity: {
-                        detectsOn: "window",
-                        events: {
-                            onClick: {
-                                enable: true,
-                                mode: "push",
-                            },
-                            onHover: {
-                                enable: true,
-                                mode: "repulse",
-                            },
-                            resize: true,
-                        },
-                        modes: {
-                            bubble: {
-                                distance: 400,
-                                duration: 2,
-                                opacity: 0.8,
-                                size: 40,
-                            },
-                            push: {
-                                quantity: 4,
-                            },
-                            repulse: {
-                                distance: 200,
-                                duration: 0.4,
-                            },
-                        },
-                    },
                     particles: {
+                        number: {
+                            value: 80,
+                            density: {
+                                enable: true,
+                                value_area: 800
+                            }
+                        },
                         color: {
-                            value: ["#BD10E0", "#B8E986", "#50E3C2", "#FFD300", "#E86363"],
+                            value: "#ff0000",
                             animation: {
                                 enable: true,
                                 speed: 20,
                                 sync: true
                             }
                         },
-                        links: {
-                            color: "#ffffff",
-                            distance: 150,
-                            enable: true,
-                            opacity: 0.5,
-                            width: 1,
-                        },
-                        collisions: {
-                            enable: true,
-                        },
-                        move: {
-                            direction: "none",
-                            enable: true,
-                            outMode: "bounce",
-                            random: false,
-                            speed: 6,
-                            straight: false,
-                        },
-                        number: {
-                            density: {
-                                enable: true,
-                                value_area: 800,
+                        shape: {
+                            type: "circle",
+                            stroke: {
+                                width: 0,
+                                color: "#000000"
                             },
-                            value: 80,
+                            polygon: {
+                                nb_sides: 5
+                            },
+                            image: {
+                                src: "https://cdn.matteobruni.it/images/particles/github.svg",
+                                width: 100,
+                                height: 100
+                            }
                         },
                         opacity: {
                             value: 0.5,
-                        },
-                        shape: {
-                            type: "circle",
+                            random: false,
+                            anim: {
+                                enable: false,
+                                speed: 3,
+                                opacity_min: 0.1,
+                                sync: false
+                            }
                         },
                         size: {
+                            value: 3,
                             random: true,
-                            value: 5,
+                            anim: {
+                                enable: false,
+                                speed: 20,
+                                size_min: 0.1,
+                                sync: false
+                            }
                         },
+                        line_linked: {
+                            enable: true,
+                            distance: 100,
+                            color: "random",
+                            opacity: 0.4,
+                            width: 1,
+                            triangles: {
+                                enable: true,
+                                color: "#ffffff",
+                                opacity: 0.1
+                            }
+                        },
+                        move: {
+                            enable: true,
+                            speed: 6,
+                            direction: "none",
+                            random: false,
+                            straight: false,
+                            out_mode: "out",
+                            attract: {
+                                enable: false,
+                                rotateX: 600,
+                                rotateY: 1200
+                            }
+                        }
                     },
-                    detectRetina: true,
-                    
+                    interactivity: {
+                        detect_on: "canvas",
+                        events: {
+                            onhover: {
+                                enable: true,
+                                mode: "repulse"
+                            },
+                            onclick: {
+                                enable: true,
+                                mode: "push"
+                            },
+                            resize: true
+                        },
+                        modes: {
+                            grab: {
+                                distance: 400,
+                                line_linked: {
+                                    opacity: 1
+                                }
+                            },
+                            bubble: {
+                                distance: 400,
+                                size: 40,
+                                duration: 2,
+                                opacity: 0.8,
+                                speed: 3
+                            },
+                            repulse: {
+                                distance: 200
+                            },
+                            push: {
+                                particles_nb: 4
+                            },
+                            remove: {
+                                particles_nb: 2
+                            }
+                        }
+                    },
+                    retina_detect: true,
+                    background: {
+                        color: "#000000",
+                        image: "",
+                        position: "50% 50%",
+                        repeat: "no-repeat",
+                        size: "cover"
+                    }
+
                 }}
             />
         </div>
