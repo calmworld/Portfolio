4143ece35befc385245e4a7263f94579e5c4041a
Sarah <sarah.alhuriz@hotmail.com>
2021-02-16 14:13:31 -0700

    final touches

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,7 +11,7 @@ export default function About(props) {
             <div className="about-text">
                 <h1 className="fancy">About</h1>
                 <p>
-                Innovative and adaptable <strong><em>Software Engineer</em></strong> with healthcare and customer service background. Strong grasp of JavaScript & Python fundamentals, React, API, AWS, SQL and NoSQL database designs. Curious with highly-developed analytical skills and real passion for solving problems and implementing effective solutions, in a dynamic, highly technical environment. Proactive collaborator with outstanding verbal and written communication skills. Proficient in computer science fundamentals, including data structures, algorithms, operating systems, shell commands, and distributed systems
+                Innovative and adaptable <strong><em>Software Engineer</em></strong> with healthcare and customer service background. Strong grasp of JavaScript and Python fundamentals, React, API, AWS, SQL and NoSQL database designs. Curious with highly-developed analytical skills and real passion for solving problems and implementing effective solutions, in a dynamic, highly technical environment. Proactive collaborator with outstanding verbal and written communication skills.
                 </p>
             </div>
         </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,7 +13,7 @@ function Home(props) {
     {
       name: "Connect4",
       description: "A two player connect4 game with a twist, No Gravity! See if you can win!",
-      tech: "Javascript, CSS, HTML5, JQuery",
+      tech: "JavaScript, CSS, HTML5, JQuery",
       link: "https://unruffled-archimedes-f20493.netlify.app/index.html",
       github: "https://github.com/calmworld/connect4.github.io",
       img: "../img/connect4.png",
@@ -29,7 +29,7 @@ function Home(props) {
     },{
       name: "InstaClone",
       description: "Collaboration Project - A social media Instagram clone",
-      tech: "MERN - MongoDB (Mongoose), Express, React, Node, AuthO, HTML/CSS, Bootstrap",
+      tech: "MERN stack (MongoDB, Express, React, Node), AuthO, HTML/CSS, Bootstrap",
       link: "https://stark-ocean-08311.herokuapp.com/",
       github: "https://github.com/cwhitney85/instaclone-client",
       img: "../img/instaclone.png",
@@ -37,7 +37,7 @@ function Home(props) {
     },{
       name: "Pancea",
       description: "Have you been feeling under the weather? Would you like to know what your body is telling you? Pancea is a symptom checker app built using physician maintained medical API",
-      tech: "MERN stack (MongoDB, Express, React, Node), HTML/CSS, Bootstrap, Infermedica API",
+      tech: "JavaScript, MERN stack (MongoDB, Express, React, Node), AWS, HTML/CSS, Bootstrap, Infermedica API",
       link: "https://main.dfzzr93qmqe2b.amplifyapp.com/",
       github: "https://github.com/calmworld/pancea-client",
       img: "../img/panceasite.png",
@@ -45,7 +45,7 @@ function Home(props) {
     }, {
       name: "NerdStop",
       description: "Trivia Game with random categories for a fun trivia night",
-      tech: "React, Materialize, Trivia API",
+      tech: "JavaScript, React, AWS, Materialize, Trivia API",
       link: "https://main.d1yvs5ruv6qdon.amplifyapp.com/",
       github: "https://github.com/calmworld/nerdstop",
       img: "../img/nerdstop.png"
