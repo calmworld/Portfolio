7430b37b7c9d8745426e5b649bb1e5c4fe80d6e5
Sarah <sarah.alhuriz@hotmail.com>
2021-02-16 14:02:36 -0700

    adjust about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,7 +11,7 @@ export default function About(props) {
             <div className="about-text">
                 <h1 className="fancy">About</h1>
                 <p>
-                Innovative and adaptable <strong><em>Software Engineer</em></strong> with a healthcare background. Driven with a strong grasp of Javascript & Python fundamentals, React, APIs, SQL and NoSQL database designs. Curious with highly-developed analytical skills and real passion for solving problems and implementing effective solutions, in a dynamic, highly technical environment. Passionate and intuitive in executive interactions and adept at building high end web applications. Exceptional collaborator with excellent communication skills. Proficient in computer science fundamentals, including data structures, algorithms, operating systems, and distributed systems.
+                Innovative and adaptable <strong><em>Software Engineer</em></strong> with healthcare and customer service background. Strong grasp of JavaScript & Python fundamentals, React, API, AWS, SQL and NoSQL database designs. Curious with highly-developed analytical skills and real passion for solving problems and implementing effective solutions, in a dynamic, highly technical environment. Proactive collaborator with outstanding verbal and written communication skills. Proficient in computer science fundamentals, including data structures, algorithms, operating systems, shell commands, and distributed systems
                 </p>
             </div>
         </div>
