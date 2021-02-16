3f1e920b158808c5e1b0f9fb9dad76d19217e618
Sarah <sarah.alhuriz@hotmail.com>
2021-02-16 13:58:30 -0700

    adjust a few details

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,7 +12,7 @@ function Home(props) {
   let projects = [
     {
       name: "Connect4",
-      description: "The classic connect4 game with a twist, No Gravity! See if you can win!",
+      description: "A two player connect4 game with a twist, No Gravity! See if you can win!",
       tech: "Javascript, CSS, HTML5, JQuery",
       link: "https://unruffled-archimedes-f20493.netlify.app/index.html",
       github: "https://github.com/calmworld/connect4.github.io",
@@ -21,7 +21,7 @@ function Home(props) {
     },{
       name: "amazing",
       description: "E-commerce website",
-      tech: "Express.js, Node.js, Mongoose, EJS, HTML, CSS, Materialize",
+      tech: "RESTful API, JavaScript, EJS, BCrypt, Express, Node, HTML/CSS, MongoDB, Materialize",
       link: "https://radiant-lowlands-77744.herokuapp.com/products",
       github: "https://github.com/calmworld/amazing.github.io",
       img: "../img/amazing.png",
@@ -29,7 +29,7 @@ function Home(props) {
     },{
       name: "InstaClone",
       description: "Collaboration Project - A social media Instagram clone",
-      tech: "MERN - MongoDB (Mongoose), Express.js, React, Node.js, Bootstrap",
+      tech: "MERN - MongoDB (Mongoose), Express, React, Node, AuthO, HTML/CSS, Bootstrap",
       link: "https://stark-ocean-08311.herokuapp.com/",
       github: "https://github.com/cwhitney85/instaclone-client",
       img: "../img/instaclone.png",
@@ -37,8 +37,7 @@ function Home(props) {
     },{
       name: "Pancea",
       description: "Have you been feeling under the weather? Would you like to know what your body is telling you? Pancea is a symptom checker app built using physician maintained medical API",
-      quote: "“Really Useful App” - Ron Meyers, GA Instructor",
-      tech: "Express, React, Mongoose, Bootstrap, Node.js",
+      tech: "MERN stack (MongoDB, Express, React, Node), HTML/CSS, Bootstrap, Infermedica API",
       link: "https://main.dfzzr93qmqe2b.amplifyapp.com/",
       github: "https://github.com/calmworld/pancea-client",
       img: "../img/panceasite.png",
@@ -46,7 +45,7 @@ function Home(props) {
     }, {
       name: "NerdStop",
       description: "Trivia Game with random categories for a fun trivia night",
-      tech: "React, Materialize",
+      tech: "React, Materialize, Trivia API",
       link: "https://main.d1yvs5ruv6qdon.amplifyapp.com/",
       github: "https://github.com/calmworld/nerdstop",
       img: "../img/nerdstop.png"
